8d7f6980c8ea3b9d95a5cc47d49c26d82367e2c0
Kleberson Garbazza <165429840+klegarbazza@users.noreply.github.com>
2025-07-13 20:10:26 -0300

    Add files via upload

---
+++ modulo4/microblog/app/models/models.py
@@ -0,0 +1,18 @@
+from app import db
+from flask_login import UserMixin
+from datetime import datetime
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    foto = db.Column(db.String(500), nullable=True)  # URL da foto de perfil
+    bio = db.Column(db.Text, nullable=True)  # Bio do usuário
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
